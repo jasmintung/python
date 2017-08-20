6596b8fd27a48bfe5953acc6e95458d1a1d517b2
jasmintung <puzexiong@163.com>
2017-08-20 23:14:58 +0800

    代码更新

--- No4WeekMission_One/userInfo/core/search_ep.py
+++ No4WeekMission_One/userInfo/core/search_ep.py
@@ -42,21 +42,22 @@ def process(index=3, *args, **kwargs):
     return search_process(index, args)
 
 
-def search_process(index, args): #不能在写*args否则形参会成为元组的元组((....))
+def search_process(index, args): #不能再写*args否则形参会成为元组的元组((....))
     result = False
-    employ_info = {}
-    print(args)
-    tmpstr = args[0].__str__()
-    print(type(tmpstr))
-    employ_info = eval(tmpstr)
-    print(type(employ_info))
     with open(settings.BASE_DB, "r" ,encoding="utf-8") as fp:
-        for epinfo in fp:
-            print(epinfo.strip('\n')) #打印并过滤每一行末尾的\n
-            tmp_dict = eval(epinfo)
-            print(type(tmp_dict))
-            if tmp_dict.get("starffId") == employ_info.get("starffId") \
-                or tmp_dict.get("name") == employ_info.get("name"):
-                print("已存在,不能增加!!")
-                result = False
+        if index == 1: # 增加
+            employ_info = {}
+            tmpstr = args[0].__str__()
+            employ_info = eval(tmpstr)
+            result = True
+            for epinfo in fp:
+                print(epinfo.strip('\n')) #打印并过滤每一行末尾的\n
+                tmp_dict = eval(epinfo)
+                print(type(tmp_dict))
+                if tmp_dict.get("starffId") == employ_info.get("starffId") \
+                    or tmp_dict.get("name") == employ_info.get("name"):
+                    print("已存在,不能增加!!")
+                    result = False
+        elif index == 2: # 删除
+
     return result
