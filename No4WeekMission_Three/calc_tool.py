63b008cb63fcb885a70b707605dc60bd57dd44f1
zhangtong <zhangtong@streamax.com>
2017-09-12 20:21:57 +0800

    正则表达式计算器

---
+++ No4WeekMission_Three/calc_tool.py
@@ -0,0 +1,89 @@
+# import re
+#
+# str = "1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + (23-3*13) - 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"
+#
+# data = re.search('\([^()]+\)', str).group()
+# print(data)
+# str1 = r"(abd1(23)"
+#
+# a = 'Beautiful, is; better*than\nugly'
+# x = re.split('\(|\)', str)
+# y = re.split(',|; |\*|\n', a)
+# print(x)
+# print(y)
+
+import re
+
+bracket = re.compile(r'\([^()]+\)')  # 寻找最内存括号规则
+mul = re.compile(r'(\d+\.?\d*\*-\d+\.?\d*)|(\d+\.?\d*\*\d+\.?\d*)')  # 寻找乘法运算规则
+div = re.compile(r'(\d+\.?\d*/-\d+\.?\d*)|(\d+\.?\d*/\d+\.?\d*)')  # 寻找除法运算规则
+add = re.compile(r'(-?\d+\.?\d*\+-\d+\.?\d*)|(-?\d+\.?\d*\+\d+\.?\d*)')  # 寻找加法运算规则
+sub = re.compile(r'(-?\d+\.?\d*--\d+\.?\d*)|(-?\d+\.?\d*-\d+\.?\d*)')  # 寻找减法运算规则
+c_f = re.compile(r'\(?\+?-?\d+\)?')  # 检查括号内是否运算完毕规则
+strip = re.compile(r'[^(].*[^)]')  # 去掉括号规则
+
+
+def Mul(s):
+    """计算表达式中的乘法运算"""
+    exp = re.split(r'\*', mul.search(s).group())
+    return s.replace(mul.search(s).group(), str(float(exp[0]) * float(exp[1])))
+
+
+def Div(s):
+    """计算表达式中的除法运算"""
+    exp = re.split(r'/', div.search(s).group())
+    return s.replace(div.search(s).group(), str(float(exp[0]) / float(exp[1])))
+
+
+def Add(s):
+    """计算表达式中的加法运算"""
+    exp = re.split(r'\+', add.search(s).group())
+    return s.replace(add.search(s).group(), str(float(exp[0]) + float(exp[1])))
+
+
+def Sub(s):
+    """计算表达式中的减法运算"""
+    exp = sub.search(s).group()
+    if exp.startswith('-'):
+        exp = exp.replace('-', '+')
+        res = Add(exp).replace('+', '-')
+    else:
+        exp = re.split(r'-', exp)
+        res = str(float(exp[0]) - float(exp[1]))
+    return s.replace(sub.search(s).group(), res)
+
+
+def calc():
+    """程序入口"""
+    while True:
+        note = input("Please input the expression(q for quit):")
+        check_left_kuohao = re.findall(r"\)*\)", note)
+        str_l_kuohao = ''.join(check_left_kuohao)
+        check_right_kuohao = re.findall(r"\(*\(", note)
+        str_r_kuohao = ''.join(check_right_kuohao)
+        if len(str_l_kuohao) != len(str_r_kuohao):
+            print("括号没配对!!!")
+            continue
+        if note == 'q':
+            break
+        else:
+            note = ''.join([x for x in re.split('\s+', note)])  # 去掉空格重组
+            if not note.startswith('('):  # 若用户输入的表达式首尾无括号,则统一格式化为:(表达式)
+                note = str('(%s)' % note)
+            while bracket.search(note):  # 若表达式note存在括号
+                note = note.replace('--', '+')  # 检查表达式,并将--运算替换为+运算
+                note_search = bracket.search(note).group()  # 得到最内层括号及其内容赋给变量note_search
+                if div.search(note_search):
+                    note = note.replace(note_search, Div(note_search))
+                elif mul.search(note_search):
+                    note = note.replace(note_search, Mul(note_search))
+                elif sub.search(note_search):
+                    note = note.replace(note_search, Sub(note_search))
+                elif add.search(note_search):
+                    note = note.replace(note_search, Add(note_search))
+                elif c_f.search(note_search):  # 检查括号内运算是否完毕
+                    note = note.replace(note_search, strip.search(note_search).group())  # 将括号去掉
+            print("The calc result is: %.2f" % (float(note)))
+
+if __name__ == '__main__':
+    calc()
