5429c3a47b7c664c61aae9e7b837f05ba6aa9103
jasmintung <puzexiong@163.com>
2018-06-03 23:33:19 +0800

    多对多关联部分

--- No7WeekMissionTwo/core/db_handler.py
+++ No7WeekMissionTwo/core/db_handler.py
@@ -30,7 +30,21 @@ class DBControle(object):
             result = TablesInit.Teacher(name=kwargs.get('name'), password=kwargs.get('password'))
             self.db.add(result)
         elif type == 2:  # 创建班级
-            self.db.add_all(kwargs.get('cl') + kwargs.get('sl'))
+            cl = kwargs.get('cl')
+            sl = kwargs.get('sl')
+            print("class obj:", cl)
+            print("student obj:", sl)
+            if len(cl) == 0 and len(sl) == 0:
+                print("没有数据不用操作数据库")
+            else:
+                if len(cl) == 0 and len(sl) != 0:
+                    print("插入S")
+                    self.db.add_all(sl)
+                elif len(cl) != 0 and len(sl) == 0:
+                    print("插入C")
+                    self.db.add_all(cl)
+                else:
+                    self.db.add_all(sl + cl)
             pass
         elif type == 3:  # 创建上课记录
             pass
@@ -48,16 +62,33 @@ class DBControle(object):
 
     def search(self, type, **kwargs):
         result = None
-        if type == 0:  # 查询成绩
-            pass
-        elif type == 1:  # 查询学员表信息
-            result = self.db.query(TablesInit.Student).all()
-        elif type == 2:  # 查询班级表信息
-            result = self.db.query(TablesInit.Class).all()
-        elif type == 3:  # 查询讲师表信息
-            result = self.db.query(TablesInit.Teacher).all()
         try:
-            self.db.commit()
+            if type == 0:  # 查询成绩
+                pass
+            elif type == 1:  # 查询学员表信息
+                result = self.db.query(TablesInit.Student).all()
+            elif type == 2:  # 查询班级表信息
+                if kwargs is None:
+                    result = self.db.query(TablesInit.Class).all()
+                else:
+                    for key in kwargs:
+                        print(key, kwargs[key])
+                        if key == 'name':
+                            result = self.db.query(TablesInit.Class).filter_by(name=kwargs[key]).first()
+            elif type == 3:  # 查询讲师表信息
+                if kwargs is None:
+                    result = self.db.query(TablesInit.Teacher).all()
+                else:
+                    for key in kwargs:
+                        print(key, kwargs[key])
+                        if key == 'name':
+                            result = self.db.query(TablesInit.Teacher).filter_by(name=kwargs[key]).first()
+                        elif key == 'id':
+                            result = self.db.query(TablesInit.Teacher).filter_by(id=kwargs[key]).first()
+                        print(result.name)
+                        print(result.id)
+                print("查询结果:", result)
+                # self.db.commit()
             return result
         except Exception as ex:
             print(ex)
@@ -71,23 +102,25 @@ class DBControle(object):
         :return:
         """
         result = None
-        if type == 0:  # 查询成绩
-            pass
-        elif type == 1:  # 查询学员表信息,根据QQ号
-            result = self.db.query(TablesInit.Student).filter_by(qq_number=kwargs.get('qq')).first()
-        elif type == 2:  # 查询班级表信息
-            result = self.db.query(TablesInit.Class).all()
-        elif type == 3:  # 查询讲师表信息
-            result = self.db.query(TablesInit.Teacher).all()
         try:
-            self.db.commit()
+            if type == 0:  # 查询成绩
+                pass
+            elif type == 1:  # 查询学员表信息,根据QQ号
+                result = self.db.query(TablesInit.Student).filter_by(qq_number=kwargs.get('qq')).first()
+            elif type == 2:  # 查询班级表信息
+                result = self.db.query(TablesInit.Class).all()
+            elif type == 3:  # 查询讲师表信息
+                result = self.db.query(TablesInit.Teacher).all()
+                # self.db.commit()
             return result
         except Exception as ex:
             print(ex)
             self.db.rollback()
 
-    def modify(self):
+    def modify(self, type):
         if type == 0:  # 修改上课记录
+            pass
+        elif type == 1:  # 修改班级信息
             pass
         try:
             self.db.commit()
---
+++ practice/test.py
@@ -0,0 +1,3 @@
+ts_dict = {'name': 'zhangtong', 'age': 28}
+for key in ts_dict:
+    print(key, ts_dict[key])
