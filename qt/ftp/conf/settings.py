0a03391b84d0fd844a9452a1574379a45418717f
jasmintung <puzexiong@163.com>
2018-07-25 16:26:59 +0800

    代码更新

---
+++ qt/ftp/conf/settings.py
@@ -0,0 +1,2 @@
+LOGIN_PAGE_SIZE = {"width": 300, "height": 200}
+LOGIN_TITLE = "登陆"
---
+++ qt/ftp/core/control/testsend.py
@@ -0,0 +1,6 @@
+import time
+
+def start_login_check(args):
+    result = "登陆完成"
+    time.sleep(3)
+    args(result)
